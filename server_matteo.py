8aecc6022c274836f06122f168d97073a6d575eb
ahliko <killian.guillemot@ynov.com>
2023-11-07 12:12:55 +0100

    Update File

--- server_matteo.py
+++ server_matteo.py
@@ -1,12 +1,12 @@
 import argparse
 import socket
 import logging
+
 import colorlog
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", action="store", type=int, default="13337", help="specify the port to connect to")
 args = parser.parse_args()
-
 
 logger = colorlog.getLogger()
 logger.setLevel(logging.INFO)
@@ -32,8 +32,6 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
-
-
 # logging.basicConfig(filename='/var/log/bs_server/bs_server.log', encoding='utf-8', level=logging.DEBUG,
 #                     format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
@@ -57,25 +55,27 @@ logger.info(f"Un client {addr[0]} s'est connecté.")
 
 response = "error"
 
-
 try:
-    while True:
-        data = conn.recv(1024)
-        if not data: break
-        logger.info(f"Le client {addr[0]} a envoyé {data.decode()}.")
-        if ("meo" in data.decode()):
-            logger.info(f"Le client {addr[0]} a envoyé 'meo'.")
-            response = "Meo à toi confrère."
-        elif ("waf" in data.decode()):
-            logger.info(f"Le client {addr[0]} a envoyé 'waf'.")
-            response = "ptdr t ki"
-        else:
-            logger.info(f"Le client {addr[0]} a envoyé autre chose.")
-            response = "Mes respects humble humain."
-except socket.error:
-    print("Erreur de connexion.")
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            logger.info(f"Le client {addr[0]} a envoyé {data.decode()}.")
+            if ("meo" in data.decode()):
+                logger.info(f"Le client {addr[0]} a envoyé 'meo'.")
+                response = "Meo à toi confrère."
+            elif ("waf" in data.decode()):
+                logger.info(f"Le client {addr[0]} a envoyé 'waf'.")
+                response = "ptdr t ki"
+            else:
+                logger.info(f"Le client {addr[0]} a envoyé autre chose.")
+                response = "Mes respects humble humain."
+    except socket.error:
+        print("Erreur de connexion.")
 
-logger.info(f"Réponse à envoyer au client {addr[0]} : {response}.")
-conn.sendall(response.encode())
-logger.info(f"Réponse envoyée au client {addr[0]} : {response}.")
-conn.close()
+    logger.info(f"Réponse à envoyer au client {addr[0]} : {response}.")
+    conn.sendall(response.encode())
+    logger.info(f"Réponse envoyée au client {addr[0]} : {response}.")
+finally:
+    conn.close()
+    s.close()
